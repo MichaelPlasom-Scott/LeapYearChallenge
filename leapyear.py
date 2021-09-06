8e043399585a884e302779f391fd6206e0a84b06
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-09-06 16:21:14 +0100

    leap year challenge and test

---
+++ leapyear.py
@@ -0,0 +1,21 @@
+# def leap_year?(date)
+#   if date % 400 == 0
+#     return true
+#   elsif date % 100 == 0 
+#     return false
+#   elsif date % 4 == 0 
+#     return true
+#   else
+#     return false
+#   end
+# end
+
+def leapyearmethod(date):
+    if date % 400 == 0:
+        return True
+    elif date % 100 == 0:
+        return False
+    elif date % 4 == 0:
+        return True
+    else: 
+        return False
